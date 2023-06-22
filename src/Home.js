e4d0aad09da3939af38caf378ad91e1612f96728
Hamza Mehmood <syz71lef@rptu.de>
2023-06-22 14:31:13 +0200

    Added errors Handling from server during data fetch

--- src/Home.js
+++ src/Home.js
@@ -5,20 +5,30 @@ const Home = () => {
 
     const[blogs, setBlogs] = useState(null);
     const[isPending, setIsPending] = useState(true);
+    const[error, setError] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        fetch('http://localhost:8000/blogss')
             .then(res => {
+                if(!res.ok) {
+                    throw Error("Could not fetch the data for that resource.");
+                }
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
+                setIsPending(false);
+                setError(null);
             })
-        setIsPending(false);
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
+            });
     }, []);
 
     return ( 
         <div className="home">
+            { error && <div>{error}</div> }
             { isPending && <div>Loading....</div> }
             { blogs && <BlogList blogs={blogs} title="Blog List" /> }
         </div>
