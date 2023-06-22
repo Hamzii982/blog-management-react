5a644d3f2c76b28225b2eca52ca500608743c25d
Hamza Mehmood <syz71lef@rptu.de>
2023-06-22 13:42:54 +0200

    Learned how to add fetch request from json server and print out data

--- src/Home.js
+++ src/Home.js
@@ -3,32 +3,21 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const[blogs, setBlogs] = useState([
-        { title: 'My New Blog', body: 'lorum ipsum....', author: 'Hamza', id: 1},
-        { title: 'Welcome Party!', body: 'lorum ipsum....', author: 'Mateen', id: 2},
-        { title: 'Web Devs Top tips', body: 'lorum ipsum....', author: 'Hamza', id: 3}
-    ]);
-
-    const [randName, setRandName] = useState('Hamza');
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const[blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        console.log("Affected");
-    }, [randName]);
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+            })
+    }, []);
 
-    // const handleClick = () => {
-    //     setName('Mehmood');
-    //     setAge(25);
-    // }
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="Blog List" handleDelete={handleDelete} />
-            <button onClick={() => setRandName('Mateen')}>Change Name</button>
-            <p>{randName}</p>
+            { blogs && <BlogList blogs={blogs} title="Blog List" /> }
         </div>
      );
 }
